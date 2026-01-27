e7af2622804cc9cc18c01d8d24a7357fe0d4a01b
Peter Marshall <petms@proton.me>
2026-01-27 17:05:33 -0500

    write ipfs blocks to output dir

--- scripts/generate-ipfs.js
+++ scripts/generate-ipfs.js
@@ -18,12 +18,17 @@ class RawDirBlockstore extends BlackHoleBlockstore {
 
 const generate = async () => {
   const distDir = path.resolve('build')
-  const blocksDir = path.resolve('ipfs-blocks');
+  const blocksDir = path.join(distDir, 'ipfs');
   const blockstore = new RawDirBlockstore(blocksDir);
 
   await fs.mkdir(blocksDir, { recursive: true });
 
-  const paths = await glob('**/*', { cwd: distDir, nodir: true });
+  const paths = await glob('**/*', {
+    cwd: distDir,
+    nodir: true,
+    ignore: ['ipfs/**']
+  });
+
   const source = await Promise.all(paths.map(async (p) => ({
     path: p,
     content: await fs.readFile(path.join(distDir, p))
