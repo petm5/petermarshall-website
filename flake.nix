abaf25b8934935317e7d80736316cab85b965c2c
Peter Marshall <pm@petermarshall.ca>
2026-08-05 04:22:56 +0000

    switch to pnpm

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Peter Marshall's homepage";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
