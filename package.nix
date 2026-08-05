8538f2494784a4b2dcc44a11eafd409a88eb1518
Peter Marshall <pm@petermarshall.ca>
2026-08-05 04:22:56 +0000

    switch to pnpm

--- package.nix
+++ package.nix
@@ -1,26 +1,40 @@
 {
-  buildNpmPackage,
-  importNpmLock,
+  stdenv,
+  nodejs,
+  fetchPnpmDeps,
+  pnpm_10,
+  pnpmConfigHook,
+  pnpmBuildHook,
   ...
 }:
 
 let
   packageJson = builtins.fromJSON (builtins.readFile ./package.json);
+
+  pnpm = pnpm_10;
 in
 
-buildNpmPackage {
+stdenv.mkDerivation (self: {
   pname = packageJson.name;
   version = packageJson.version;
   src = ./.;
 
-  npmDeps = importNpmLock {
-    npmRoot = ./.;
+  pnpmDeps = fetchPnpmDeps {
+    inherit (self) pname version src;
+    inherit pnpm;
+    fetcherVersion = 4;
+    hash = "sha256-BBmQltd9CvWWZL4yMuBb6iglYUblVoGMwEGfTB+p1FE=";
   };
 
-  npmConfigHook = importNpmLock.npmConfigHook;
+  nativeBuildInputs = [
+    nodejs
+    pnpmConfigHook
+    pnpmBuildHook
+    pnpm
+  ];
 
   installPhase = ''
-    mkdir -p $out/share/nginx/html
-    cp -r build/* $out/share/nginx/html
+    mkdir -p $out
+    cp -r build/* $out
   '';
-}
+})
