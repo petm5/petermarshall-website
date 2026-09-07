f7daf50cdc785287c1caf0761eefb1d034bad180
Peter Marshall <pm@petermarshall.ca>
2026-09-07 17:31:56 -0400

    add maskable icon

--- src/routes/manifest.webmanifest/+server.js
+++ src/routes/manifest.webmanifest/+server.js
@@ -2,6 +2,8 @@ import { title, tagline } from '$lib/site.json'
 
 import icon_192 from '$lib/assets/app-icon.png?w=192&format=avif&quality=90'
 import icon_512 from '$lib/assets/app-icon.png?w=512&format=avif&quality=90'
+import icon_maskable_192 from '$lib/assets/app-icon-maskable.png?w=192&format=avif&quality=90'
+import icon_maskable_512 from '$lib/assets/app-icon-maskable.png?w=512&format=avif&quality=90'
 
 export const prerender = true
 
@@ -29,8 +31,20 @@ const manifest = {
       src: icon_512,
       sizes: "512x512",
       type: "image/avif"
+    },
+    {
+      src: icon_maskable_192,
+      purpose: "maskable",
+      sizes: "192x192",
+      type: "image/avif"
+    },
+    {
+      src: icon_maskable_512,
+      purpose: "maskable",
+      sizes: "512x512",
+      type: "image/avif"
     }
-    ],
+  ],
   display: "minimal-ui"
 }
 
