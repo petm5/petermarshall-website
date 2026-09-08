1d023fcc9467a778a00bf4b2ff95df8b19bccd4f
Peter Marshall <pm@petermarshall.ca>
2026-09-08 17:51:38 -0400

    amino: wait a bit before shutting down the node

--- src/lib/ipfs/amino-provider.ts
+++ src/lib/ipfs/amino-provider.ts
@@ -79,6 +79,8 @@ export class IpfsProvider {
     } catch (err) {
       throw err
     } finally {
+      // Wait for peers to verify our submission
+      await new Promise(res => setTimeout(res, 10 * 1000))
       await node.stop()
     }
 
